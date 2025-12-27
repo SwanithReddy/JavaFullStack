f4c0522cb6bc27d8ec19b0c0f6b3e3da4127a5af
SwanithReddy <swanithreddi@gmail.com>
2025-12-27 21:29:21 +0530

    added variables folder

---
+++ Variables/Datatypes.java
@@ -0,0 +1,31 @@
+package JavaFullStack.Variables;
+
+public class Datatypes {
+
+    public static void main(String[] args) {
+
+        int a=5;
+        float f=5.0;
+        short s=42;
+        byte b=6;
+        long l=21500;
+        char c='h';
+        double d=5.6;
+
+        System.out.println(a);
+        System.out.println(f);
+
+        System.out.println(s);
+
+        System.out.println(b);
+        System.out.println(l);
+        System.out.println(c);
+        System.out.println(d);
+
+
+
+
+
+
+    }
+}
---
+++ Variables/Variables.java
@@ -0,0 +1,14 @@
+package JavaFullStack.Variables;
+
+public class Variables {
+    
+    public static void main(String[] args) {
+        int a;
+
+        a=10;
+        
+
+        System.out.println(a);
+    }
+
+}
